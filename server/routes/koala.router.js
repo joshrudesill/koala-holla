a400e1d534d887c5d6d46ff7163637a98269c110
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-02-01 11:23:49 -0500

    imported pg;created db connection

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pg = require('pg'); //import pg
 
-// DB CONNECTION
-
+// DB CONNECTION--POOL
+const pool = new pg.Pool({
+    database: "koalas",
+    host: "localhost",
+    port: 5432,
+});
 
 // GET
 
